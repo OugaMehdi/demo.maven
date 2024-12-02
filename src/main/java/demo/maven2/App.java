92c301bb409c9edd943fbb7c2a33be008803f3e5
OugaMehdi <95387941+OugaMehdi@users.noreply.github.com>
2024-12-02 16:47:02 +0100

    Add files via upload

---
+++ src/main/java/demo/maven2/App.java
@@ -0,0 +1,12 @@
+package demo.maven2;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello !!");
+	}
+	
+	public static int addition(int a, int b) {
+		return a+b;
+	}
+}
